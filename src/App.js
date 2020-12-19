5c92abaa3dd78914dbd5629b5c0fb6d80af431e3
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-19 15:41:10 +0100

    Created a new apoloclient

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { ApolloClient, InMemmoryCache, InMemoryCache } from "@apollo/client";
+
 
 function App() {
+
+  const client = new ApolloClient({
+    uri: 'https://48p1r2roz4.sse.codesandbox.io',
+    cache: new InMemoryCache()
+  });
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Welcome to GraphQL Client </h1>
     </div>
   );
 }
