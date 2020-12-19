a36b33529f10d31b745ea0b2f822c4aecb11980e
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-19 16:20:23 +0100

    Fetching currency rate working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 
 import './App.css';
-import { ApolloClient, InMemmoryCache, InMemoryCache } from "@apollo/client";
-
+import { 
+  ApolloClient, InMemoryCache, gql, ApolloProvider,
+  useQuery
+} 
+  from "@apollo/client";
+import React from 'react';
 
 function App() {
 
@@ -10,12 +14,40 @@ function App() {
     cache: new InMemoryCache()
   });
 
+  const EXCHANGE_RATES = gql`
+    query GetExchangeRates {
+        rates(currency: "USD") {
+            currency
+            rate
+        }
+    }
+`;
+
+  function ExchangeRates() {
+    const { loading, error, data } = useQuery(EXCHANGE_RATES);
+    if (loading) return <p>Loading to obtain data from graphql server</p>
+    if (error) return <p> Error in obtaining data.... :(</p>;
+
+    return data.rates.map(({ currency, rate }) => (
+      <div key={currency} className="currency">
+        <p className="currency__rate">
+          {currency}: {rate}
+        </p>
+      </div>
+    ));
+  }
+
   
 
   return (
-    <div className="app">
-      <h1>Welcome to GraphQL Client </h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="app">
+        <h1>Welcome to GraphQL Client </h1>
+      </div>
+      <p className="heading">Currency Rate</p>
+      <ExchangeRates />
+    </ApolloProvider>
+    
   );
 }
 
